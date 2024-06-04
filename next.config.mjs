eab5211035e5809ee5009fdbb80365b7296bef09
patnana lakshmana murthy <patnanalakshmanamurthy9@gmail.com>
2024-06-04 08:03:41 +0530

    redirect problem test

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,28 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-},
+  },
 
   images: {
     remotePatterns: [
       {
-        
         hostname: "images.unsplash.com",
-        
       },
       {
-        hostname:"https://media.licdn.com"
-      }
+        hostname: "https://media.licdn.com",
+      },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/api/linkedin/authorize',
+        destination: `https://linkedin.com/oauth/v2/authorization?client_id=${process.env.CLIENT_ID}&response_type=code&scope=${process.env.SCOPE}&redirect_uri=${process.env.OAUTH2_REDIRECT_URL}`,
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 };
-
 
 export default nextConfig;
